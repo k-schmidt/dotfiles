1f503807b337e1b5c2c6ff4434ce64a566caec23
Kyle Schmidt <kyleschmidt@Kyles-Mac-mini.local>
2026-01-29 20:47:12 -0800

    add initial iterm2 plist

---
+++ run_once_install_zsh_plugins.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 1. Install Oh My Zsh (unattended) if it's missing
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Define the custom directory (defaults to OMZ custom)
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+# 2. Install Powerlevel10k Theme
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  echo "Installing Powerlevel10k..."
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+fi
+
+# 3. Install Zsh Autosuggestions
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  echo "Installing zsh-autosuggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+# 4. Install Zsh Syntax Highlighting
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  echo "Installing zsh-syntax-highlighting..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
