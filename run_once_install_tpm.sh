ac3bdc74c3bca47b6d9d2c27a22737cc6297a5d1
Kyle <kyle.a.schmidt@gmail.com>
2026-05-03 17:08:06 -0700

    fix: make run_once scripts non-fatal when github.com is unreachable
    Devvms can't resolve github.com. Add || echo fallbacks so chezmoi
    apply continues instead of aborting on clone failures.

--- run_once_install_tpm.sh
+++ run_once_install_tpm.sh
@@ -6,5 +6,5 @@ mkdir -p ~/.tmux/plugins
 # Clone TPM if it doesn't already exist
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     echo "Installing Tmux Plugin Manager..."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || echo "Skipping TPM install (github.com unreachable)"
 fi
